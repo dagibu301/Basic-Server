3f6170e5b83a82f64bf60523a7b2d03908935efb
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-17 21:06:10 -0500

    Hello World

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const htttp = require('http');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.end('<h1>Hello World!</h1>');
+});
+
+server.listen(3000);
