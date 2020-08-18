348dfd117a4c5c8bec279b1ee9244ae7c71f48d4
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-17 21:10:53 -0500

    user

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Hello World!</h1>');
+    console.log('Method: ', request.method);
+    console.log('Url: ', request.url);
+    const user = {
+        name: 'John',
+        lastName: 'Doe'
+    }
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 });
 
 server.listen(3000);
