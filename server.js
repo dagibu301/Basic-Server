637cae90d1addfb3c8e3a092962545230adb3d29
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-17 21:34:57 -0500

    middleware header

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     const user = {
         name: 'John',
         lastName: 'Doe'
+    }
+    res.send(user);
+});
+
+app.post('/profiles', (req,res) => {
+    console.log(req.body);
+    const user = {
+        name: 'John2',
+        lastName: 'Doe2'
     }
     res.send(user);
 });
