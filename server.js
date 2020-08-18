1993848e52ba6a4df32b002e4de3b3d8edfe5fe6
David Giraldo <davidgiraldobueno@gmail.com>
2020-08-18 17:22:51 -0500

    fs

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(express.static(__dirname + 'public'));
 
-app.get('/', (req, res) => {
-    const user = {
-        name: 'John',
-        lastName: 'Doe'
-    }
-    res.send(user);
+app.get("/", (req, res) => {
+  const user = {
+    name: "John",
+    lastName: "Doe",
+  };
+  res.send(user);
 });
 
-app.post('/profiles', (req,res) => {
-    console.log(req.body);
-    const user = {
-        name: 'John2',
-        lastName: 'Doe2'
-    }
-    res.send(user);
+app.post("/profiles", (req, res) => {
+  console.log(req.body);
+  const user = {
+    name: "John2",
+    lastName: "Doe2",
+  };
+  res.send(user);
 });
 
 app.listen(3000);
